c6c85d9c0cb4498d79a84c973c6140eeb7497d1f
pinglend <wes@getpinglend.com>
2022-08-31 14:09:48 -0700

    WIP gradient background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({animationClass: 'test'})
+
+  const changeState = () => {
+    if(state.animationClass === 'test'){
+      setState({
+        animationClass: 'test clicked'
+      });
+    }else{
+      setState({
+        animationClass: 'test'
+      });
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={state.animationClass}>
+      <button onClick={changeState}>Stop / Start</button>
     </div>
   );
 }
