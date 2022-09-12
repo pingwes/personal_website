0c88a36b276053c757fdb71904464b7092e4f8ea
pinglend <wes@getpinglend.com>
2022-09-11 21:55:50 -0700

    Working on transitions

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ function App() {
         value={"about"}  
           onClick={e => handleChange(e) } >About</button> */}
         {renderPage()}
+        
       </div>
     </>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,41 @@
 
 import React from "react";
-  
+import { CSSTransition } from 'react-transition-group';
+import { useState } from 'react';
+
 const Home = ({handleChange}) => {
+  const [showMessage, setShowMessage] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+
   return (
-    <div className="mx-5 py-20 text-right text-4xl">
-    <button
-      className="my-5" 
-      value={"about"}  
-      onClick={e => handleChange(e) } >About</button>
-    
-      <div className="my-5">Blog</div>
-      <div className="my-5">Projects</div>
-      <div className="my-5">Artwork</div>
-      <div className="my-5">LinkedIn</div>
-    </div>
+    <>
+      <button
+          onClick={() => setShowMessage(true)}
+          size="lg">
+          Show Message
+        </button>
+      <CSSTransition
+        in={showMessage}
+        timeout={300}
+        classNames="alert"
+        unmountOnExit
+        onEnter={() => setShowButton(false)}
+        onExited={() => setShowButton(true)}
+      >      
+      <div className="mx-5 py-20 text-right text-4xl">
+        <button
+          className="my-5" 
+          value={"about"}  
+          onClick={e => handleChange(e) } >About</button>
+        
+          <div className="my-5">Blog</div>
+          <div className="my-5">Projects</div>
+          <div className="my-5">Artwork</div>
+          <div className="my-5">LinkedIn</div>
+        </div>
+        
+      </CSSTransition>
+    </>
     );
 };
 
