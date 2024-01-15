f8e84981a9dce5054ab4b6da3bf7bc13dee72d29
pinglend <wes@getpinglend.com>
2024-01-14 20:36:44 -0500

    Update bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const About = (handleChange) => {
       </div> */}
       <div className="text-sm">
         <ul>
-          <li>I'm a software engineer living in San Francisco, California. I studied computer science at Michigan State University. In my free time, I like to prototype technologies, listen to music, paint, play guitar and piano, and enjoy nature.</li>
+          <li>I'm Co-founder and CTO of Prophetic AI. I live in New York City. In my free time, I like to prototype technologies, listen to music, paint, play guitar and piano, and enjoy nature.</li>
         </ul>
       </div>
     </div>
